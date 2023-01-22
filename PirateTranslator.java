52b42fc7aacffe53ca1064520cf36e3b148f64ca
Tylern84 <94724113+Tylern84@users.noreply.github.com>
2023-01-21 17:38:43 -0800

    Add files via upload

---
+++ PirateTranslator.java
@@ -0,0 +1,32 @@
+public class PirateTranslator {
+
+    String[] phrases = {"hello", "his", "hers", "hi", "is", "pardon me", "excuse me",
+			"my", "friend", "sir", "madam",
+			"stranger", "officer",
+			"where", "you", "tell",
+			"know", "how far", "old", "happy"};
+    String[] piratetalk = {"ahoy", "ye", "ye", "yo-ho-ho", "be", "avast", "arrr",
+			   "me", "me bucko", "matey", "proud beauty",
+			   "scurvy dog", "foul blaggart",
+			   "whar", "ye", "be tellin'",
+			   "be knowin'", "how many leagues",
+			   "barnacle-covered", "grog-filled"};
+	
+    String[] positiveWords = {"adore", "enjoy", "love"};
+    String[] negativeWords = {"hate", "despise", "dislike"};
+    
+    String[] lastTranslations = new String[25];
+    int s = 0;
+
+    /**
+    * _Part 1: Implement this method_
+    *
+    * Translate the input string and return the resulting string
+    */ 
+    public String translate(String input) {
+	// TODO: implement this
+	return null;
+    }
+
+    
+}
